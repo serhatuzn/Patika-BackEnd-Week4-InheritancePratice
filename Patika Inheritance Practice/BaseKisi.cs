b68a62df9a8167c45624f43111398cfad8f39cb4
Serhatuzn <seerhatuzun@gmail.com>
2024-10-27 14:57:30 +0300

    Inheritance Practice

--- Patika Inheritance Practice/BaseKisi.cs
+++ Patika Inheritance Practice/BaseKisi.cs
@@ -8,12 +8,34 @@ namespace Patika_Inheritance_Practice
     public class BaseKisi
     {
         // burda hem öğretmende hem öğrencide kullanacağımız bilgileri oluşturuyoruz
-        public string Ad { get; set; } 
+        public string Ad { get; set; }
         public string Soyad { get; set; }
 
         public void BaseYazdır()
         {
             Console.WriteLine($"Ad: {Ad} \r\nSoyad: {Soyad}"); // Ad soyad şeklinde Yazdırıyoruz
+        }
+    }
+
+    public class Ogrenci : BaseKisi
+    {
+        public int OgrNO { get; set; } // Öğrencinin Sınıf no'su için oluşturduk
+
+        public void OgrenciYazdır()
+        {
+            Console.WriteLine($"Öğrenci No : {OgrNO}"); // Öğrenicnin Sınıf No'sunu Yazdırdık.
+            BaseYazdır(); // Base classını Yazdırdık.
+        }
+    }
+
+    public class Ogretmen : BaseKisi
+    {
+        public int Maaş { get; set; } // Öğretmenin maaşı için oluşturduk
+
+        public void OgretmenYazdır()
+        {
+            Console.WriteLine($"Maaş : {Maaş}"); // Maaşı Yazdırıyoruz.
+            BaseYazdır(); // Base Classında oluşturdumuz bilgileri yazdırdık.
         }
     }
 }
--- Patika Inheritance Practice/obj/Debug/net8.0/Patika Inheritance Practice.AssemblyInfo.cs
+++ Patika Inheritance Practice/obj/Debug/net8.0/Patika Inheritance Practice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Patika Inheritance Practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c55738861919083f7e85210ace345777037bab6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c791398c01e832819d1f8792a80cbcc93dacb39")]
 [assembly: System.Reflection.AssemblyProductAttribute("Patika Inheritance Practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Patika Inheritance Practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
